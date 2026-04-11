14e9468ecc430b12d932c3ff37eff161162f5b9e
Luke Hodges <luke@theironheart.org>
2026-04-11 13:48:41 +0100

    fix(turbopack): turbopack had set address to tobys drive resulting in build error

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  turbopack: {
-    root: "C:\\Users\\tobyd\\PC\\Desktop\\UOB-trackr"
-  }
 };
 
 export default nextConfig;
